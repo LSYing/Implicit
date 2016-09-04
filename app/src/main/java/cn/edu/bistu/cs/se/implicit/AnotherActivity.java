c0ba04f7c69a23c77522b9f70845fce743cb9b82
LSYing <312458788@qq.com>
2016-09-04 22:52:21 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/bistu/cs/se/implicit/AnotherActivity.java
@@ -0,0 +1,16 @@
+package cn.edu.bistu.cs.se.implicit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by lsy on 2016/9/4.
+ */
+public class AnotherActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.another_layout);
+
+    }
+}
